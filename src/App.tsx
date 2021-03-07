be80981395fb4328cedf8f0f65c62ef76675b72e
rodrigocamargo <rodrigocamargo854@gmail.com>
2021-03-07 16:48:56 -0300

    error in types

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import '../styles/App;scss'
 function App() {
   return (
     <div className="App">
-      <Editor/>
-     
+      <Editor />
     </div>
   );
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,25 +1,60 @@
 
+import { stringify } from "node:querystring"
 import React, { useState } from "react"
 import "../styles/editor.scss"
 
 export default function () {
 
+    const [panelWidth, SetpanelWidth] = useState(16)
+    const [panelHeight, SetpanelHeight] = useState(16)
+    const [hideOptions, SethideOptions] = useState(false)
+    const [hideDrawingPanel, SethideDrawingPanel] = useState(true)
+    const [butoonText, SetbutoonText] = useState("start drawing")
+    const [selectorColor, SetselectColor] = useState('#f44336')
+
+
+interface initializeDrawingPanelProps{
+
+}
+
+    function initializeDrawingPanel(props: initializeDrawingPanelProps) {
+        SethideOptions(!hideOptions)
+        SethideDrawingPanel(!hideDrawingPanel)
+
+        butoonText == "start drawing"
+            ? SetbutoonText('reset')
+            : SetbutoonText("start drawing")
+    }
+
     return (
         <div id='editor'>
             <h1>Editor de Pixels</h1>
-            <h2>Entre com as dimensões do painel</h2>
+            {hideDrawingPanel && <h2> Entre com as dimensões do painel</h2>}
+            {hideDrawingPanel && (
 
-            <div id='options'>
-                <div className='option'>
-                    <input type="number" className='panelInput' />
-                    <span> Largura </span>
-                </div>
-                <div className='option'>
-                    <input type="number" className='panelInput' />
-                    <span> Altura </span>
+                <div id='options'>
+                    <div className='option'>
+                        <input 
+                        type="number" 
+                        className='panelInput'
+                        defaultValue={panelWidth}
+                        onChange={(e)=> {
+
+                            SetpanelWidth(e.target.value)
+
+                        }}
+                        />
+                        <span> Largura </span>
+                    </div>
+                    <div className='option'>
+                        <input type="number" className='panelInput' />
+                        <span> Altura </span>
+                    </div>
                 </div>
 
-            </div>
+            )}
+
+
             <button className='button'> Começar a Desenhar</button>
         </div>
     )
